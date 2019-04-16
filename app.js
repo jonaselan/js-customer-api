b98078362b01aa07015a6dd7a13f3563ff351d8d
jonaselan <jonas.elan@gmail.com>
2019-04-16 18:13:39 -0300

    Install more recent version of elastic

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let db = require('./db.js');
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'http://localhost:9200' });
 
 // Set up the express app
 const app = express();
@@ -11,7 +13,7 @@ app.get('/', function(req, res) {
 });
 
 // get all messages
-app.get('/api/conversation/messages', (req, res) => {
+app.get('/api/conversation/random_messages', (req, res) => {
     res.status(200).send({
         success: 'true',
         data: db.getAllMessages()
@@ -38,3 +40,51 @@ const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 });
+
+console.log('qqqqqqqqq');
+
+async function run () {
+    // Let's start by indexing some data
+    await client.index({
+        index: 'api-customers-js',
+        type: 'messages',
+        body: {
+            "id": 1,
+            "from": 7,
+            "to": 4,
+            "read": false,
+            "content": "Omnis mollitia sint et dolores ut commodi.",
+            "created_at": "2019-03-28 12:53:04",
+            "updated_at": "2019-03-28 12:53:04"
+        }
+    });
+
+    await client.index({
+        index: 'api-customers-js',
+        type: 'users',
+        body: {
+            "id":10,
+            "name":"Tad Windler",
+            "email":"ugerhold@example.com",
+            "email_verified_at":null,
+            "created_at":"2019-03-28 12:52:39",
+            "updated_at":"2019-03-28 12:52:39"
+        }
+    });
+
+    // Let's search!
+    const { body } = await client.search({
+        index: 'game-of-thrones',
+        body: {
+            query: {
+                match: {
+                    quote: 'winter'
+                }
+            }
+        }
+    });
+
+    console.log(body.hits.hits)
+}
+
+run().catch(console.log)
